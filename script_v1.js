fa137a8d96177776d93577b863b3f2572f63b6d9
Torrey Meade <themeadeconcept@hotmail.com>
2021-03-04 19:30:47 -0500

    Refactored the Javascript for dynamic Input's and error outputs - by using functions

--- script_v1.js
+++ script_v1.js
@@ -27,25 +27,35 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
-
-// Get field name
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
 // Event Listener - listen for submit - pass in event parameter
 form.addEventListener("submit", function (e) {
   // Makes the form not actually submit
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
+  // not having the .value property will log the element, not the actual input
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
+  } else {
+    showSuccess(password2);
+  }
 });
