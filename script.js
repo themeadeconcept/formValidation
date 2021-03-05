93e36a47dc30dd8126f9f0b771a864a653470bc6
Torrey Meade <themeadeconcept@hotmail.com>
2021-03-04 19:14:01 -0500

    Initial commit - Validation usuing if statements and REGEX

---
+++ script.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show Input error Message
+function showError(input, message) {
+  // parentElement will grab the parent element of the input
+  const formControl = input.parentElement;
+
+  // className will overwrite existing classes for form-control, therefore we must include form-control class again along with error
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Event Listener - listen for submit - pass in event parameter
+form.addEventListener("submit", function (e) {
+  // Makes the form not actually submit
+  e.preventDefault();
+
+  // not having the .value property will log the element, not the actual input
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
+  } else {
+    showSuccess(password2);
+  }
+});
